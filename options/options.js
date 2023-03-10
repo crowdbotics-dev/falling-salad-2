0faf7a5500081e695e23db18b4fbec4ea9b02bed
Crowdbotics <engineering@crowdbotics.com>
2023-03-10 16:50:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-salad-2",
+  url: "https://falling-salad-2.botics.co",
+  api: "https://falling-salad-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
